dd3e092944f7a63d854ec3ea0afe182e9643757a
sootn <encrypt.wave@gmail.com>
2015-03-17 15:03:45 +0100

    Testing out of order (or is it the other way around?)

--- Makefile
+++ Makefile
@@ -7,7 +7,18 @@ runtests: testsources/main_io_read testsources/main_ooo_read testsources/testrun
 testsources/main_io_read: testsources/main_io_read.c
 	$(compile) testsources/main_io_read.c -o testsources/main_io_read
 
+testsources/main_ooo_read: testsources/main_ooo_read.c
+	$(compile) testsources/main_ooo_read.c -o testsources/main_ooo_read
+
 smallest: testsources/smallest_possible_io.ll
 
 testsources/smallest_possible_io.ll:
 	$(clang) -O1 -std=c11 -g -S -emit-llvm testsources/smallest_possible_io.c -o testsources/smallest_possible_io.ll
+
+realmisses1:
+	cat datastream | ./testsources/main_ooo_read
+
+# Start instruments in between
+
+realmisses2:
+	cat testdata > datastream
---
+++ testsources/main_ooo_read.c
@@ -0,0 +1,45 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef long long intish;
+
+typedef struct Matrix {
+  int nrows;
+  int ncols;
+  intish* data;
+} Matrix;
+
+__attribute__ ((noinline))
+Matrix multiply(Matrix A, Matrix B) {
+  Matrix C = {A.nrows, B.ncols, (intish *) malloc(sizeof(intish) * A.nrows * B.ncols)};
+  for(int column = 0; column < C.ncols; column++) {
+    for(int row = 0; row < C.nrows; row++) {
+      intish s = 0;
+      for(int k = 0; k < A.ncols; k++) {
+        intish a = A.data[k + row * A.ncols];
+        intish b = B.data[column + k * B.ncols];
+        s += a * b;
+      }
+      C.data[column + row * C.ncols] = s;
+    }
+  }
+  return C;
+}
+
+Matrix readMatrix() {
+  int nrows, ncols;
+  scanf("%d", &nrows);
+  scanf("%d", &ncols);
+  intish *data = (intish*) malloc(nrows * ncols * sizeof(intish));
+  for(intish i = 0; i < nrows * ncols; i++) {
+    scanf("%lld", &data[i]);
+  }
+  Matrix r = {ncols, nrows, data};
+  return r;
+}
+
+int main() {
+  Matrix A = readMatrix();
+  Matrix B = readMatrix();
+  multiply(A, B);
+}
